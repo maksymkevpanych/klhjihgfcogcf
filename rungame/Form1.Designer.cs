32ad528bd786ae542b0b569ea541cf9051243268
maksymkevpanych <129380088+maksymkevpanych@users.noreply.github.com>
2023-05-07 22:04:18 +0300

    Add files via upload

---
+++ rungame/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace rungame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.character = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.character)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // character
+            // 
+            this.character.Image = ((System.Drawing.Image)(resources.GetObject("character.Image")));
+            this.character.Location = new System.Drawing.Point(60, 135);
+            this.character.Name = "character";
+            this.character.Size = new System.Drawing.Size(87, 126);
+            this.character.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.character.TabIndex = 0;
+            this.character.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(238, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 126);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(656, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "current HP:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.character);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.character)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox character;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ rungame/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
+using System.Security.Cryptography.X509Certificates;
+
+namespace rungame
+{
+    public partial class Form1 : Form
+    {
+        public int currentIndex1 = 0;
+        public int currentIndex2 = 0;
+        public int currentIndex3 = 0;
+        public int characterHP = 3;
+        bool spikes;
+        public string[] arr = { "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (1).png",
+            "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (2).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (3).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (4).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (5).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (6).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (7).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (8).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (9).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (10).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (11).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (12).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (13).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (14).png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder1\\Run (15).png"};
+        public string[] spikeArr = { "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder2\\long_wood_spike_01.png",
+        "C:\\Users\\maxke\\OneDrive\\Робочий стіл\\task\\rungame\\rungame\\NewFolder2\\long_wood_spike_05.png"};
+        Thread t;
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+
+            character.Image = System.Drawing.Image.FromFile(arr[currentIndex1]);
+            timer1.Interval = 100;
+            pictureBox1.Image= System.Drawing.Image.FromFile(spikeArr[currentIndex2]);
+            
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        
+        
+
+        
+
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            currentIndex1++;
+            currentIndex3++;
+            if (currentIndex1 == arr.Length)
+            {
+                currentIndex1 = 0;
+            }
+            character.Image = System.Drawing.Image.FromFile(arr[currentIndex1]);
+            if (currentIndex3 == 15)
+            {
+                currentIndex2++;
+                if (currentIndex2 == 1)
+                {
+                    spikes = true;
+                }
+                if (currentIndex2 != 1)
+                {
+                    spikes=false;
+                }
+                if (currentIndex2 == spikeArr.Length)
+                {
+                    currentIndex2 = 0;
+                }
+                pictureBox1.Image = System.Drawing.Image.FromFile(spikeArr[currentIndex2]);
+                currentIndex3 = 0;
+            }
+        }
+       
+        public void isCharacterAlive()
+        {
+            int characterX = character.Left;
+            int characterY = character.Top;
+            int characterWidth = character.Width;
+            int characterHeight = character.Height;
+            int spike1X = pictureBox1.Left;
+            int spike1Y = pictureBox1.Top;
+            int spike1Width = pictureBox1.Width;
+            int spike1Height = pictureBox1.Height;
+            Rectangle characterRect = new Rectangle(characterX, characterY, characterWidth/2, characterHeight/2);
+            Rectangle spike1Rect = new Rectangle(spike1X, spike1Y, spike1Width/2, spike1Height/2);
+            if (characterRect.IntersectsWith(spike1Rect))
+            {
+                if (spikes == true)
+                {
+                    characterHP--;
+                    label1.Text = $"currentHP={characterHP}";
+                }
+                
+                
+            }
+            if (characterHP == 0)
+            {
+                this.Close();
+                t = new Thread(open);
+                t.Start();
+            }
+
+        }
+        public void open(object sender)
+        {
+            System.Windows.Forms.Application.Run(new Form2());
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.W)
+            {
+                character.Top -= 10; // adjust the value to change the speed of movement
+            }
+
+           
+            if (e.KeyCode == Keys.S)
+            {
+                character.Top += 10; // adjust the value to change the speed of movement
+            }
+
+            
+            if (e.KeyCode == Keys.A)
+            {
+                character.Left -= 10; // adjust the value to change the speed of movement
+            }
+
+           
+            if (e.KeyCode == Keys.D)
+            {
+                character.Left += 10; // adjust the value to change the speed of movement
+            }
+            isCharacterAlive();
+        }
+    }
+}
